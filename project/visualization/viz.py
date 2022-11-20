87fcaf2efecda9c3f895f18b22a29c6b803f4201
ThibaultF02 <thibaultferretti@gmail.com>
2022-11-21 00:26:25 +0100

    minor speed upgrade

--- project/visualization/viz.py
+++ project/visualization/viz.py
@@ -6,40 +6,33 @@ import plotly.express as px
 import json
 from dash import Dash, dcc, html, Input, Output
 
-pd.set_option('display.max_columns', None)
 
-# Data cleaning
-# TODO look for missing data, separate per year
-
+# Downloading data
 url = 'https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download'
 data_path = os.path.join(os.getcwd(), 'data_viz.csv')
 path = download(url, data_path, progressbar=True, verbose=True)
 
-df = pd.read_csv(data_path, sep=";")
-# Keeping useful columns, dropping duplicates, renaming for convenience
-df = df.loc[:, df.columns.intersection(['annee', 'code_commune', 'nom_commune',
-                                        'consommation_annuelle_moyenne_de_la_commune_mwh'])]
+# Dataframe creation, keeping only useful columns
+df = pd.read_csv(data_path, sep=";", usecols=[0, 7, 8, 13])
+
+# Dropping duplicates, renaming for convenience
 df.drop_duplicates(inplace=True)
 df = df.rename(columns={"code_commune": "code", "nom_commune": "nom",
                         "consommation_annuelle_moyenne_de_la_commune_mwh": "conso"})
 
 
-
+# TODO fix path names with os
 city = '../../data/communes.geojson'
 dept = '../../data/departements.geojson'
 region = '../../data/regions.geojson'
 
-
 cities = json.load(open(city, 'r'))
-for feature in cities['features']:
-    feature["code_commune"] = feature["properties"]["code"]
 
 # Plots
 # TODO Violin plot per year per city
 
 # Interactive map
 # TODO define a class to get max min per region, dept
-
 
 # App layout
 app = Dash(__name__)
@@ -80,15 +73,17 @@ def update_graph(option_slctd):
     dff = df.copy()
     dff = dff[dff["annee"] == option_slctd]
 
-    fig = px.choropleth_mapbox(dff.head(20),
+    # Map not showing anything at >=474, memory/cache problem?
+    n = 473
+    fig = px.choropleth_mapbox(dff.head(n),
                                geojson=cities,
                                color="conso",
                                locations="code",
                                featureidkey="properties.code",
                                mapbox_style="carto-positron",
                                hover_data=['conso'],
-                               zoom=4,
-                               center={"lat": 46, "lon": 2},
+                               zoom=3.7,
+                               center={"lat": 47, "lon": 2},
                                opacity=0.6,
                                )
 
