5824618d20146c41ae5ed2bf4aa8f8bef5b8eaf1
ThibaultF02 <thibaultferretti@gmail.com>
2022-12-03 20:46:35 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from Trackelec import __version__ as current_version
+
+setup(
+  name='trackelec',
+  version=current_version,
+  description='Visualization and prediction of the french electricity consumption',
+  url='https://github.com/mle-seach/hax712x_project.git',
+  author='Thibault FERRETTI, Sarah MATOUB, Pauline DUSFOUR-CASTAN, Mathieu LE-SEACH, Guillaume BERNARD-REYMOND',
+  author_email='thibault.ferretti@etu.umontpellier.fr',
+  license='MIT',
+  packages=['trackelec','trackelec.predic', 'trackelec.visu'],
+  zip_safe=False
+)
