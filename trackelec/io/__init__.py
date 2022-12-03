6590cd5bb6e73cce49c4845b887df146d95effad
ThibaultF02 <92214175+ThibaultF02@users.noreply.github.com>
2022-12-04 00:43:57 +0100

    fix case

--- trackelec/io/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-url_dataviz = "https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download"
-path_target = os.path.join(os.getcwd(), "trackelec", "visu", "data_viz.csv")
