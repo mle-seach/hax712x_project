992406abae98e96c710587054ccdc4994abef5cf
ThibaultF02 <thibaultferretti@gmail.com>
2022-12-05 13:49:35 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
 import trackelec as track
 
-df = track.load_dataviz().save_as_df()
+df_viz = track.load_dataviz().save_as_df()
+df_cons = track.load_datacons().save_as_df()
+df_cons2022 = track.load_datacons2022().save_as_df()
